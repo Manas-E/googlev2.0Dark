bfc1e94216ddfc2d39c971a4ab62d53f5bc2c4d6
Manas Sharma <manazharma@gmail.com>
2021-10-25 21:36:49 +0530

    add google login

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className='grid w-full divide-y-[1px] divide-gray-300
          bg-gray-100 text-sm text-gray-500'>
-            <div className=''>
+            <div className='flex justify-center items-center'>
                 <p>India</p>
             </div>
 
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBW5ZllJYGcSdGa7cUDCUZZYe7q0WnOwiE",
+  authDomain: "fir-demo-402d1.firebaseapp.com",
+  projectId: "fir-demo-402d1",
+  storageBucket: "fir-demo-402d1.appspot.com",
+  messagingSenderId: "789814590396",
+  appId: "1:789814590396:web:dde8d7702cc6637f5b49b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
